7aa48a0ca3686dbf57c7ee55aba52a6802f5a178
akash-tqb <akashtqb06@gmail.com>
2025-02-25 15:02:39 +0530

    Feat removed images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,37 @@
-import type React from "react";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "next-themes";
-import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Footer } from "@/components/footer";
-import SparklesCore from "@/components/ui/particles";
+import  Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Mavenberg Innovations",
-  description: "Migration-Integration-Aggregation-Reporting",
-  generator: "akashinsights.com",
+export const metadata: Metadata = {
+  title: "Mavenberg Innovation",
+  description:
+    "UI Template",
   icons: {
-    icon: "/favicon.ico",
-  },
+    icon: "/favicon.ico"
+  }
 };
 
 export default function RootLayout({
   children
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Navbar />
-          <SparklesCore
-            background="transparent"
-            minSize={0.6}
-            maxSize={1.4}
-            particleDensity={1}
-            className="fixed inset-0 -z-50 w-full h-full bg-transparent pointer-events-none"
-          />
-          <main>
+          <main className="flex-grow">
             <div className="relative overflow-hidden">{children}</div>
           </main>
           <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,15 @@ import FeaturesSection from "@/components/features-section"
 import LandingPage from "@/components/landing-page"
 import HeroSection from "@/components/hero-section"
 import FAQAccordion from "@/components/faq-accordion"
-import SparklesCore from "@/components/ui/particles"
-import {Footer} from "@/components/footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen overflow-x-hidden flex-col items-center justify-start">
+    <>
       <LandingPage />
       <FeaturesSection />
       <FAQAccordion/>
       <HeroSection/>
-    </main>
+    </>
   )
 }
 
